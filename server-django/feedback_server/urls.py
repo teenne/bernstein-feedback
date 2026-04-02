69b372922f7bf54c2d9373540d9847ea29fcbfd5
kajalTripathi910 <kt@akkomplish.com>
2026-04-02 19:02:28 +0530

    refactor: remove legacy feedback server django code and related files

--- server-django/feedback_server/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    # Support both /api/feedback and /api/feedback/
-    path('api/feedback', include('feedback.urls')),
-    path('api/feedback/', include('feedback.urls')),
-    path('health', include('feedback.health_urls')),
-]
